5bc917a6b369aa0367815e4004e71e9bbc9d055f
marceloaires7 <marceloaires7>
2024-07-04 22:30:05 +0000

    EXERCÍCIO 05 ATUALIZADO

--- dia05/EXERCICIO_DIA05.sql
+++ dia05/EXERCICIO_DIA05.sql
@@ -124,131 +124,76 @@ ORDER BY idVendedor, descTipoPagamento
 
 -- 4. Combine a query do exercício 2 e 3 de tal forma, que cada linha seja um vendedor, e que haja colunas para cada meio de pagamento (com a quantidade de pedidos) e as colunas das estatísticas do pedido do exercício 2 (média, maior valor e menor valor).
 
-WITH tb_creditcard AS (
+WITH tb_pedido_receita AS (
 
-  SELECT t1.idVendedor,
-         t2.descTipoPagamento AS creditCard,
-         COUNT(DISTINCT t1.idPedido) AS qtdCreditCard
-  FROM silver.olist.item_pedido AS t1
-  LEFT JOIN silver.olist.pagamento_pedido AS t2
-  ON t1.idPedido = t2.idPedido
-  LEFT JOIN silver.olist.pedido AS t3
-  ON t1.idPedido = t3.idPedido
-  WHERE t2.descTipoPagamento = 'credit_card'
-  AND DATE(t3.dtPedido) >= '2017-01-01'
-  AND DATE(t3.dtPedido) <= '2017-06-30'
-  GROUP BY t1.idVendedor, t2.descTipoPagamento
-
-),
-
-tb_debitcard AS (
-
-  SELECT t1.idVendedor,
-         t2.descTipoPagamento AS debitCard,
-         COUNT(DISTINCT t1.idPedido) AS qtdDebitCard
-  FROM silver.olist.item_pedido AS t1
-  LEFT JOIN silver.olist.pagamento_pedido AS t2
-  ON t1.idPedido = t2.idPedido
-  LEFT JOIN silver.olist.pedido AS t3
-  ON t1.idPedido = t3.idPedido
-  WHERE t2.descTipoPagamento = 'debit_card'
-  AND DATE(t3.dtPedido) >= '2017-01-01'
-  AND DATE(t3.dtPedido) <= '2017-06-30'
-  GROUP BY t1.idVendedor, t2.descTipoPagamento
-
-),
-
-tb_boleto AS (
-
-  SELECT t1.idVendedor,
-         t2.descTipoPagamento AS boleto,
-         COUNT(DISTINCT t1.idPedido) AS qtdBoleto
-  FROM silver.olist.item_pedido AS t1
-  LEFT JOIN silver.olist.pagamento_pedido AS t2
-  ON t1.idPedido = t2.idPedido
-  LEFT JOIN silver.olist.pedido AS t3
-  ON t1.idPedido = t3.idPedido
-  WHERE t2.descTipoPagamento = 'boleto'
-  AND DATE(t3.dtPedido) >= '2017-01-01'
-  AND DATE(t3.dtPedido) <= '2017-06-30'
-  GROUP BY t1.idVendedor, t2.descTipoPagamento
-
-),
-
-tb_voucher AS (
-
-  SELECT t1.idVendedor,
-         t2.descTipoPagamento AS voucher,
-         COUNT(DISTINCT t1.idPedido) AS qtdVoucher
-  FROM silver.olist.item_pedido AS t1
-  LEFT JOIN silver.olist.pagamento_pedido AS t2
-  ON t1.idPedido = t2.idPedido
-  LEFT JOIN silver.olist.pedido AS t3
-  ON t1.idPedido = t3.idPedido
-  WHERE t2.descTipoPagamento = 'voucher'
-  AND DATE(t3.dtPedido) >= '2017-01-01'
-  AND DATE(t3.dtPedido) <= '2017-06-30'
-  GROUP BY t1.idVendedor, t2.descTipoPagamento
-
-),
-
-tb_valores AS (
-
-  SELECT idVendedor,
-         AVG(vlPreco) AS avgPreco,
-         MAX(vlPreco) AS maxPreco,
-         MIN(vlPreco) AS minPreco,
-         COUNT(DISTINCT t1.idPedido) AS qtdPedidos
-  FROM silver.olist.item_pedido AS t1
-  LEFT JOIN silver.olist.pedido AS t2
-  ON t1.idPedido = t2.idPedido
-  WHERE DATE(t2.dtPedido) >= '2017-01-01'
-  AND DATE(t2.dtPedido) <= '2017-06-30'
-  GROUP BY idVendedor
-
-),
-
-tb_final AS (
-  SELECT DISTINCT t2.idVendedor,
-         t3.creditCard,
-         t3.qtdCreditCard,
-         t4.debitCard,
-         t4.qtdDebitCard,
-         t5.boleto,
-         t5.qtdBoleto,
-         t6.voucher,
-         t6.qtdVoucher,
-         t7.avgPreco,
-         t7.minPreco,
-         t7.maxPreco,
-         t7.qtdPedidos
+  SELECT t2.idPedido,
+         t2.idVendedor,
+         SUM(t2.vlPreco) AS vlReceita
 
   FROM silver.olist.pedido AS t1
 
   INNER JOIN silver.olist.item_pedido AS t2
   ON t1.idPedido = t2.idPedido
 
-  LEFT JOIN tb_creditcard AS t3
-  ON t2.idVendedor = t3.idVendedor
-
-  LEFT JOIN tb_debitcard AS t4
-  ON t2.idVendedor = t4.idVendedor
-
-  LEFT JOIN tb_boleto AS t5
-  ON t2.idVendedor = t5.idVendedor
-
-  LEFT JOIN tb_voucher AS t6
-  ON t2.idVendedor = t6.idVendedor
-
-  LEFT JOIN tb_valores AS t7
-  ON t2.idVendedor = t7.idVendedor
-
   WHERE DATE(t1.dtPedido) >= '2017-01-01'
-  AND DATE(t1.dtPedido) <= '2017-06-30' 
+  AND DATE(t1.dtPedido) <= '2017-06-30'
+
+  GROUP BY t2.idPedido, t2.idVendedor
+
+),
+
+-- Tabela temporária do exercício 02
+tb_sumario_pedido AS (
+
+  SELECT idVendedor,
+        AVG(vlReceita) AS avgValorPedido,
+        MIN(vlReceita) AS minValorPedido,
+        MAX(vlReceita) AS maxValorPedido
+
+  FROM tb_pedido_receita
+
+  GROUP BY idVendedor
+
+),
+
+-- Tabela temporária do exercício 03
+tb_pedido_pagamento AS (
+
+  SELECT t1.idVendedor,
+         t2.descTipoPagamento,
+         COUNT(DISTINCT t1.idPedido) AS qtdPedido
+
+  FROM tb_pedido_receita AS t1
+
+  LEFT JOIN silver.olist.pagamento_pedido AS t2
+  ON t1.idPedido = t2.idPedido
+
+  GROUP BY t1.idVendedor, t2.descTipoPagamento
+
+  ORDER BY t1.idVendedor, t2.descTipoPagamento
+),
+
+tb_pagamento_coluna AS (
+
+  SELECT idVendedor,
+        SUM(CASE WHEN descTipoPagamento = 'boleto' THEN qtdPedido END) AS qtdBoleto,
+        SUM(CASE WHEN descTipoPagamento = 'credit_card' THEN qtdPedido END) AS qtdCredit_card,
+        SUM(CASE WHEN descTipoPagamento = 'voucher' THEN qtdPedido END) AS qtdVoucher,
+        SUM(CASE WHEN descTipoPagamento = 'debit_card' THEN qtdPedido END) AS qtdDebit_card
+
+  FROM tb_pedido_pagamento
+
+  GROUP BY idVendedor
+
 )
 
-SELECT *
+SELECT t1.*,
+       qtdBoleto,
+       qtdCredit_card,
+       qtdVoucher,
+       qtdDebit_card
 
-FROM tb_final
+FROM tb_sumario_pedido AS t1
 
-ORDER BY qtdPedidos DESC
+LEFT JOIN tb_pagamento_coluna AS t2
+ON t1.idVendedor = t2.idVendedor
